022fad34947b26566d31221804d71999e4924d5a
Fruna4942 <kdlrlgusk@naver.com>
2022-01-02 23:35:50 +0900

    first commit

---
+++ src/test/java/com/toyproject/discord_clone/DiscordCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyproject.discord_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
